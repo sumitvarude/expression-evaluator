7b3be814d8bfa7ea628b9f296cc70d4ef58ab648
sumitvarude <sumitvarude@gmail.com>
2014-01-24 22:50:45 +0530

    spaceHandler method created

--- src/sumitvar/evaluator/lib/EvaluatorOperations.java
+++ src/sumitvar/evaluator/lib/EvaluatorOperations.java
@@ -61,7 +61,6 @@ public class EvaluatorOperations {
     }
 
 
-
     public String joinExpression(String[] arg) {
         StringBuilder joinedExpression = new StringBuilder();
         for (int i = 0; i < arg.length; i++) {
@@ -72,21 +71,24 @@ public class EvaluatorOperations {
 
     public String reduceSpaces(String arg) {
         String fileData = arg;
-        fileData = fileData.replaceAll("[ ]+", "");
+        fileData = fileData.replaceAll("[ ]+", " ");
         return fileData;
     }
-    public String getExpressionFromBrackets(String arg,int startIndex,int endIndex){
-        String expression  = arg;
-        String expressionInBracket = expression.substring(startIndex,endIndex);
+
+    public String getExpressionFromBrackets(String arg, int startIndex, int endIndex) {
+        String expression = arg;
+        String expressionInBracket = expression.substring(startIndex, endIndex);
         return expressionInBracket;
     }
-    public String[] swapArrayElementsByOne(String[]arg){
-        String temp[] = new String[arg.length-1];
-        for (int i = 0; i < arg.length-1; i++) {
-            temp[i] = arg[i+1];
+
+    public String[] swapArrayElementsByOne(String[] arg) {
+        String temp[] = new String[arg.length - 1];
+        for (int i = 0; i < arg.length - 1; i++) {
+            temp[i] = arg[i + 1];
         }
         return temp;
     }
+
     public float evaluate(String arg) {
         arg = arg.trim();
         String[] elementList = arg.split(" ");
@@ -112,30 +114,49 @@ public class EvaluatorOperations {
     }
 
     public float evaluateExpression(String wholeExpression) {
+        wholeExpression = handleSpaces(wholeExpression);
         float finalResult = 0.0f;
-        int indexOfOpeningBracket=-1,indexOfClosingBracket=-1;
+        int indexOfOpeningBracket = -1, indexOfClosingBracket = -1;
         wholeExpression = wholeExpression.trim();
         String[] elementList = wholeExpression.split("");
-        if(true == wholeExpression.contains("(")){
+        if (true == wholeExpression.contains("(")) {
             for (int i = 0; i < wholeExpression.length(); i++) {
-                if(wholeExpression.charAt(i)=='('){
+                if (wholeExpression.charAt(i) == '(') {
                     indexOfOpeningBracket = i;
                 }
-                if(wholeExpression.charAt(i)==')'){
+                if (wholeExpression.charAt(i) == ')') {
                     indexOfClosingBracket = i;
                     break;
                 }
             }
-            String expressionFromBrackets = getExpressionFromBrackets(wholeExpression,indexOfOpeningBracket+2,indexOfClosingBracket-1);
+            String expressionFromBrackets = getExpressionFromBrackets(wholeExpression, indexOfOpeningBracket + 2, indexOfClosingBracket - 1);
             float result = evaluate(expressionFromBrackets.trim());
 
-            String modifiedExpression = wholeExpression.replace("( "+expressionFromBrackets+" )",String.valueOf(result));
+            String modifiedExpression = wholeExpression.replace("( " + expressionFromBrackets + " )", String.valueOf(result));
             return evaluateExpression(modifiedExpression);
-        }
-        else{
+        } else {
             finalResult = evaluate(wholeExpression);
         }
         return finalResult;
+    }
+
+    public String handleSpaces(String arg) {
+        arg = arg.trim();
+        arg = arg.replaceAll("\\+", " + ");
+        arg = arg.replaceAll("\\-", " - ");
+        arg = arg.replaceAll("\\*", " * ");
+        arg = arg.replaceAll("\\^", " ^ ");
+        arg = arg.replaceAll("\\(", " ( ");
+        arg = arg.replaceAll("\\)", " ) ");
+        arg = arg.replaceAll("/", " / ");
+        arg = arg.replaceAll("  - ", " -");
+        arg = arg.replaceAll(" -  "," -");
+        arg = reduceSpaces(arg);
+        arg = arg.trim();
+        if (arg.charAt(0) == '-' && arg.charAt(1) == ' ')
+            arg = arg.replaceFirst("- ", "-");
+
+        return arg;
     }
 }
 
