dabaf018901f3e967238ba93aebdf7af746892cf
sumitvarude <sumitvarude@gmail.com>
2014-02-07 14:15:50 +0530

    BinaryOperations interface added

---
+++ src/sumitvar/evaluator/lib/BinaryOperation.java
@@ -0,0 +1,8 @@
+package sumitvar.evaluator.lib;
+
+/**
+ * Gives common operate method for all binary operators
+ */
+public interface BinaryOperation {
+    public double operate(double number1, double number2);
+}
