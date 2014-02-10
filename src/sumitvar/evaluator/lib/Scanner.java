cea2bbd2efe46b8505eac24c27ebc781214b87ea
sumitvarude <sumitvarude@gmail.com>
2014-02-10 11:04:24 +0530

    Scanner refactored: removed if from while loop

--- src/sumitvar/evaluator/lib/Scanner.java
+++ src/sumitvar/evaluator/lib/Scanner.java
@@ -6,10 +6,10 @@ import java.util.Arrays;
 public class Scanner {
     private String expression;
     int index;
-    String[] symbols = {"+","-","*","/","^","(",")"};
-    String[] numbers = {"0","1","2","3","4","5","6","7","8","9","."};
-    List<String> numberList = Arrays.asList(numbers);
-    List<String> operatorsList = Arrays.asList(symbols);
+    Character[] symbols = {'+','-','*','/','^','(',')'};
+    Character[] numbers = {'0','1','2','3','4','5','6','7','8','9','.'};
+    List<Character> numberList = Arrays.asList(numbers);
+    List<Character> operatorsList = Arrays.asList(symbols);
     public Scanner(String expression) {
         this.expression = expression;
         index = 0;
@@ -24,31 +24,26 @@ public class Scanner {
     public String next() {
         if(!hasNext())
             return null;
-        String nextCharacter = expression.substring(index, index + 1);
-        if(nextCharacter.equals(" ")){
+        Character nextCharacter = expression.charAt(index);
+        if(nextCharacter.equals(' ')){
             index++;
             return next();
         }
         if(operatorsList.contains(nextCharacter)){
             index++;
-            return nextCharacter;
+            return nextCharacter.toString();
         }
-        if(numberList.contains(nextCharacter)){
-            int indexForNumbers = index;
-            while (numberList.contains(nextCharacter)){
-                indexForNumbers = indexForNumbers + 1;
-                if(indexForNumbers > expression.length()-1){
-                    String lastNumber =  expression.substring(index);
-                    index = indexForNumbers;
-                    return lastNumber;
-                }
-                nextCharacter = expression.substring(indexForNumbers,indexForNumbers+1);
+        int indexForNumbers = index;
+        while(numberList.contains(nextCharacter)){
+            indexForNumbers = indexForNumbers + 1;
+            if(indexForNumbers > expression.length()-1){
+                break;
             }
-            String number = expression.substring(index,indexForNumbers);
-            index = indexForNumbers;
-            return number;
+            nextCharacter = expression.charAt(indexForNumbers);
         }
-        return null;
+        String number = expression.substring(index,indexForNumbers);
+        index = indexForNumbers;
+        return number;
     }
 }
 
